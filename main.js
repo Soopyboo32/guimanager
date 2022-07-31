49562bc29ef827a184b867a58c8477c873e6b907
Soopyboo32 <49228220+Soopyboo32@users.noreply.github.com>
2022-07-31 16:41:44 +0800

    add slider

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ import ProgressBar from './GuiElement/ProgressBar.js';
 import SoopyImageElement from './GuiElement/SoopyImageElement';
 import SoopyGui from "./SoopyGui";
 import SoopyOpenGuiEvent from "./EventListener/SoopyOpenGuiEvent"
+import Slider from "./GuiElement/Slider";
 
 let testGui = new SoopyGui();
 
@@ -25,9 +26,9 @@ let box = new SoopyBoxElement().setLocation(0.25, 0.25, 0.5, 0.5).setScrollable(
 
 testGui.element.addChild(box)
 
-box.addEvent(new SoopyOpenGuiEvent().setHandler(()=>{
-    box.location.size.y.set(0,0)
-    box.location.size.y.set(0.5,500)
+box.addEvent(new SoopyOpenGuiEvent().setHandler(() => {
+    box.location.size.y.set(0, 0)
+    box.location.size.y.set(0.5, 500)
 }))
 
 box.addChild(new SoopyTextElement().setText("§0Hello World!").setMaxTextScale(100).setLocation(0.1, 0.05, 0.8, 0.3))
@@ -37,35 +38,39 @@ let y = 0.4
 let textBox = new PasswordInput().setLocation(0.1, y, 0.8, 0.2)
 textBox.text.placeholder = "Password input"
 box.addChild(textBox)
-y+=0.2
+y += 0.2
 let textBox2 = new TextBox().setLocation(0.1, y, 0.8, 0.2)
 textBox2.text.placeholder = "Textbox"
 box.addChild(textBox2)
-y+=0.2
+y += 0.2
 let textBox3 = new NumberTextBox().setLocation(0.1, y, 0.8, 0.2)
 textBox3.text.placeholder = "Number input"
 box.addChild(textBox3)
-y+=0.2
+y += 0.2
 let textBox4 = new NumberTextBox().setLocation(0.1, y, 0.8, 0.2)
 textBox4.text.placeholder = "Number input2"
 box.addChild(textBox4)
-y+=0.2
+y += 0.2
 
-let toggle = new Toggle().setLocation(0.1,y,  0.8, 0.2)
-y+=0.2
+let toggle = new Toggle().setLocation(0.1, y, 0.8, 0.2)
+y += 0.2
 
-let loading = new BoxWithLoading().setLocation(0.1,y,  0.8, 0.2)
-y+=0.2
+let loading = new BoxWithLoading().setLocation(0.1, y, 0.8, 0.2)
+y += 0.2
 
 box.addChild(toggle)
 box.addChild(loading)
 
+let slider = new Slider().setLocation(0.1, y, 0.8, 0.2)
+y += 0.2
+box.addChild(slider)
+
 let markUp = new SoopyMarkdownElement().setLocation(0.1, y, 0.8, 0.2).setText("Hello\n#### _WORLD_\nThis is __COOL__\n\n# Right?")
 box.addChild(markUp)
-y+=markUp.getHeight()
+y += markUp.getHeight()
 
-let loading2 = new BoxWithLoading().setLocation(0.1,y,  0.8, 0.2).setLore(["Hello this is some HOVER LORE","","That is so pog right","It should even have §6COLORS"])
-y+=0.2
+let loading2 = new BoxWithLoading().setLocation(0.1, y, 0.8, 0.2).setLore(["Hello this is some HOVER LORE", "", "That is so pog right", "It should even have §6COLORS"])
+y += 0.2
 
 
 box.addChild(loading2)
@@ -75,20 +80,20 @@ let imageThing = new SoopyImageElement().setLocation(0.1, y, 0.8, 0.2).setImage(
 let preHeight = y
 
 box.addChild(imageThing)
-imageThing.onImageHeightChange(()=>{
-    y=preHeight + imageThing.location.size.y.get()
-    
-    progressBar.location.location.y.set(y)
-    y+= 0.2
-}, {})
-y+=0.2
+imageThing.onImageHeightChange(() => {
+    y = preHeight + imageThing.location.size.y.get()
 
-let progressBar = new ProgressBar().setLocation(0.1,y,  0.8, 0.2).setProgress(0.3)
-y+=0.2
+    progressBar.location.location.y.set(y)
+    y += 0.2
+}, {})
+y += 0.2
+
+let progressBar = new ProgressBar().setLocation(0.1, y, 0.8, 0.2).setProgress(0.3)
+y += 0.2
 
 box.addChild(progressBar)
 
-register("step", ()=>{
+register("step", () => {
     progressBar.setProgress(Math.random())
 }).setFps(1)
 
